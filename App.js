a6f55edb047e5659872e6124ba44e5dfceb8528c
Tunahan Çeler <tunaceler@gmail.com>
2024-09-04 21:29:46 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
+import Me from './src/screens/Me';
+import Plans from './src/screens/Plans';
+import Flights from './src/screens/Flights';
+
+
+
+// Tab navigatörü oluşturuluyor
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen name="Plans" component={Plans} />
+      <Tab.Screen name="Flights" component={Flights} />
+        <Tab.Screen name="Me" component={Me} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
---
+++ src/screens/Flights.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Flights() {
+  return (
+    <View  className="bg-red-500">
+      <Text>TÜRK HAVA</Text>
+    </View>
+  )
+}
